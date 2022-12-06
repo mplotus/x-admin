4c80dbcae922f64af66f78b817ad902e4498e4ad
mplotus <mplotus.github.io@gmail.com>
2022-12-06 14:51:12 +0700

    Update main page

--- $code/script.js
+++ $code/script.js
@@ -13,14 +13,17 @@ const pass_signin = () => {
         document.getElementById('tx_pw').value = '';
         document.getElementById('tx_pw').placeholder = 'Signing in, please wait...';
         setTimeout(() => {
-            
+            let _address = window.location.href.replace('index.html','');
+            window.location.href = _address + 'main.html';
         }, 2000);
     }
     else {
         document.getElementById('tx_pw').value = '';
+        document.getElementById('tx_pw').style.boxShadow = '0 0 15px red';
         document.getElementById('tx_pw').placeholder = 'Invalid administrator password';
         setTimeout(() => {
             document.getElementById('tx_pw').focus();
+            document.getElementById('tx_pw').style.boxShadow = 'none';
             document.getElementById('tx_pw').placeholder = 'Enter administrator password';
         }, 2000);
     }
