5d4e76ef0115bb771b3dd4e6932e96eba2bf4e48
mplotus <mplotus.github.io@gmail.com>
2022-12-06 11:01:13 +0700

    Update sign in form

--- $code/script.js
+++ $code/script.js
@@ -1,9 +1,27 @@
 const page_load = () => {
-    fetch('./googledrive/data.xml').then(res => {
-        res.text().then(xml => {
-            let _parser = new DOMParser();
-            let _root = _parser.parseFromString(xml, 'application/xml').querySelector('root');
+    
+}
+var _passHint = 'SOS binary morse code';
+var _passAct = '000111000';
+const forgot_pw = () => {
+    if(document.getElementById('tx_pw').placeholder != _passHint) 
+        document.getElementById('tx_pw').placeholder = _passHint;
+    else document.getElementById('tx_pw').placeholder = 'Enter administrator password';
+}
+const pass_signin = () => {
+    if(document.getElementById('tx_pw').value == _passAct) {
+        document.getElementById('tx_pw').value = '';
+        document.getElementById('tx_pw').placeholder = 'Signing in, please wait...';
+        setTimeout(() => {
             
-        })
-    });
+        }, 2000);
+    }
+    else {
+        document.getElementById('tx_pw').value = '';
+        document.getElementById('tx_pw').placeholder = 'Invalid administrator password';
+        setTimeout(() => {
+            document.getElementById('tx_pw').focus();
+            document.getElementById('tx_pw').placeholder = 'Enter administrator password';
+        }, 2000);
+    }
 }
